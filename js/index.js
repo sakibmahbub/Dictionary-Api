6fd6e2014523b1a2ef27862be1e6c016105c6a51
sakibmahbub <arafatsakib48@gmail.com>
2023-03-01 16:20:56 +0600

    Showed error message for invalid word search

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 const handleSearch = async () => {
   const inputValue = document.getElementById("input-field").value;
-  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`;
-  const res = await fetch(url);
-  const data = await res.json();
-  console.log(data);
+  if (inputValue) {
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    if (data.title === "No Definitions Found") {
+      alert(data.message);
+    }
+  } else {
+    alert("Please enter your text");
+  }
 };
